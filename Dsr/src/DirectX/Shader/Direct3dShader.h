398f0e8871b870ec353ce1476e78e4e310b40cfe
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2023-11-08 13:35:55 +0100

    constant buffers for shaders (Todo

--- Dsr/src/DirectX/Shader/Direct3dShader.h
+++ Dsr/src/DirectX/Shader/Direct3dShader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "DirectX/Buffers/Direct3dBuffer.h"
+
 namespace dsr
 {
 	namespace directX
@@ -11,14 +13,22 @@ namespace dsr
 			std::shared_ptr<TShader> GetShaderPtr() const { return m_shader; }
 			std::shared_ptr<ID3DBlob> GetShaderBlob() const { return m_shaderBlob; }
 
+			void AddConstantBuffer(const std::string& id, const Direct3dBuffer& buffer);
+
 			Direct3dShader(const std::shared_ptr<TShader>& shader, const std::shared_ptr<ID3DBlob>& shaderBlob)
 				: m_shader(shader), m_shaderBlob(shaderBlob)
 			{
 			}
-
 		private:
 			std::shared_ptr<TShader> m_shader;
 			std::shared_ptr<ID3DBlob> m_shaderBlob;
+			std::vector<std::pair<std::string, Direct3dBuffer>> m_constantBuffers;
 		};
+
+		template<class TShader>
+		inline void Direct3dShader<TShader>::AddConstantBuffer(const std::string& id, const Direct3dBuffer& buffer)
+		{
+			m_constantBuffers.push_back(std::make_pair<std::string, Direct3dBuffer>(id, buffer));
+		}
 	}
 }
