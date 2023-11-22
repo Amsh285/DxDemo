3d557ed3e5732ab0beccfe10d054e853233f7cfb
Dorian Bach <ai22m002@technikum-wien.at>
2023-11-22 23:35:56 +0100

    - draft dsrapplication class

--- Dsr/src/DsrApplication.cpp
+++ Dsr/src/DsrApplication.cpp
@@ -3,5 +3,41 @@
 
 namespace dsr
 {
+	void DsrApplication::WindowManager::OnWindowDestroy(const events::WindowDestroyEvent& destroyEvent)
+	{
+		using namespace dsr::windows;
 
+		std::shared_ptr<WindowApplication> application = WindowApplication::Get();
+		application->ShutDown();
+	}
+
+	void DsrApplication::WindowManager::OnWindowResize(const events::WindowResizedEvent& resizeEvent)
+	{
+	}
+
+	DsrApplication::WindowManager::WindowManager(
+		const std::shared_ptr<windows::Window>& window,
+		const std::shared_ptr<directX::Direct3dDevice>& device)
+		: m_window(window), m_device(device)
+	{
+	}
+
+	void DsrApplication::Initialize()
+	{
+		m_device = directX::Direct3dDevice::Create(m_window);
+		m_renderer = std::make_shared<dsr::directX::rendering::Direct3dRenderer>(m_device);
+
+		m_windowManager = std::make_shared<WindowManager>(m_window, m_device);
+
+		// Additional initialization logic if needed
+	}
+
+	DsrApplication::DsrApplication(
+		const std::wstring& title,
+		const int& x, const int& y,
+		const int& width, const int& height)
+		: m_window(std::make_shared<windows::Window>(windows::WindowData(title, x, y, width, height))),
+		m_windowApplication(windows::WindowApplication::Get())
+	{
+	}
 }
--- Dsr/src/DsrApplication.h
+++ Dsr/src/DsrApplication.h
@@ -1,15 +1,46 @@
 #pragma once
 
+#include "Events/EventListener.h"
+#include "Events/Application/WindowEvents.h"
+
+#include "Windows/Window.h"
+#include "Windows/WindowApplication.h"
+
+#include "DirectX/Direct3dDevice.h"
+#include "DirectX/Rendering/Direct3dRenderer.h"
+
 namespace dsr
 {
 	class DsrApplication
 	{
 	public:
+		void Initialize();
+
+		DsrApplication(
+			const std::wstring& title,
+			const int& x, const int& y,
+			const int& width, const int& height);
 	protected:
+		std::shared_ptr<windows::Window> m_window;
+		std::shared_ptr<directX::Direct3dDevice> m_device;
+		std::shared_ptr<directX::rendering::Direct3dRenderer> m_renderer;
+		std::shared_ptr<windows::WindowApplication> m_windowApplication;
+
 	private:
 		class WindowManager
 		{
 		public:
+			void OnWindowDestroy(const events::WindowDestroyEvent& destroyEvent);
+			void OnWindowResize(const events::WindowResizedEvent& resizeEvent);
+
+			WindowManager(
+				const std::shared_ptr<windows::Window>& window,
+				const std::shared_ptr<directX::Direct3dDevice>& device);
+			WindowManager(const WindowManager& other) = delete;
+			WindowManager& operator=(const WindowManager& other) = delete;
+		private:
+			std::shared_ptr<windows::Window> m_window;
+			std::shared_ptr<directX::Direct3dDevice> m_device;
 		};
 
 		std::shared_ptr<WindowManager> m_windowManager;
