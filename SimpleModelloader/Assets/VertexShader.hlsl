106dffde7478b45840d78d366cafc5b7d226b841
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2023-12-12 09:13:53 +0100

    - calculate tangent and bitangent

--- SimpleModelloader/Assets/VertexShader.hlsl
+++ SimpleModelloader/Assets/VertexShader.hlsl
@@ -47,17 +47,25 @@ VertexShaderOutput main(AppData IN)
     output.texCoord = IN.texCoord;
     output.position = mul(mvp, float4(IN.position, 1.0f));
 
+    float3 edge1 = ddx(IN.position);
+    float3 edge2 = ddy(IN.position);
+    float2 deltaUV1 = ddx(IN.texCoord);
+    float2 deltaUV2 = ddy(IN.texCoord);
+
+    float f = 1.0 / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+    float3 tangent = f * (deltaUV2.y * edge1 - deltaUV1.y * edge2);
+    float3 biTangent = f * (-deltaUV2.x * edge1 + deltaUV1.x * edge2);
+
+    float3 T = normalize(float3(mul(worldMatrix, float4(tangent, 0.0f))));
+    float3 B = normalize(float3(mul(worldMatrix, float4(biTangent, 0.0f))));
+    float3 N = normalize(float3(mul(worldMatrix, float4(IN.normal, 0.0f))));
+    float3x3 TBN = float3x3(T, B, N);
+
     return output;
 }
 
-// Todo:
-//Whenever we apply a non - uniform scale(note: a uniform scale only changes the normal's magnitude, not its direction, which is easily fixed by normalizing it) the normal vectors are not perpendicular to the corresponding surface anymore which distorts the lighting.
-//
-//    The trick of fixing this behavior is to use a different model matrix specifically tailored for normal vectors.This matrix is called the normal matrix and uses a few linear algebraic operations to remove the effect of wrongly scaling the normal vectors.If you want to know how this matrix is calculated I suggest the following article.
-//
-//    The normal matrix is defined as 'the transpose of the inverse of the upper-left 3x3 part of the model matrix'.Phew, that's a mouthful and if you don't really understand what that means, don't worry; we haven't discussed inverse and transpose matrices yet.Note that most resources define the normal matrix as derived from the model - view matrix, but since we're working in world space (and not in view space) we will derive it from the model matrix.
-//
-//    In the vertex shader we can generate the normal matrix by using the inverse and transpose functions in the vertex shader that work on any matrix type.Note that we cast the matrix to a 3x3 matrix to ensure it loses its translation properties and that it can multiply with the vec3 normal vector :
+
 
 
 //shadows
