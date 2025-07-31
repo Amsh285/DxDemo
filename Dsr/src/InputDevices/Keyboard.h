7b40727c4e5d8a20cbef1990dc542061ac25353f
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2025-07-31 23:53:23 +0200

    - some comments for future improvements

--- Dsr/src/InputDevices/Keyboard.h
+++ Dsr/src/InputDevices/Keyboard.h
@@ -24,7 +24,78 @@ namespace dsr
 
 			void PrepareNextFrame();
 		private:
-			uint16_t m_currentState, m_previousState;
+			uint16_t m_currentState, m_previousState; // change to std::bitset
+
+			// possible overflow when oring multiple keystates on IsKey...
+			// next time use this:
+			/*std::bitset<256> keyStates;
+
+			// On KeyDown:
+			keyStates.set(static_cast<size_t>(keycode), true);
+
+			// On KeyUp:
+			keyStates.set(static_cast<size_t>(keycode), false);
+			Und zum Abfragen einfach:
+
+			if (keyStates.test(static_cast<size_t>(keycode))) {
+				// Key is pressed
+			}
+			
+			*/
+/*namespace dsr
+{
+	namespace inputdevices
+	{
+		class Keyboard : public dsr::events::EventListener
+		{
+		public:
+			Keyboard() = default;
+
+			void OnKeyDown(const dsr::events::KeyDownEvent& keyDown)
+			{
+				keyStates.set(static_cast<size_t>(keyDown.GetKeyCode()), true);
+			}
+
+			void OnKeyUp(const dsr::events::KeyUpEvent& keyUp)
+			{
+				keyStates.set(static_cast<size_t>(keyUp.GetKeyCode()), false);
+			}
+
+			void OnLooseFocus(const dsr::events::LooseFocusEvent& looseFocus)
+			{
+				keyStates.reset();
+				prevKeyStates.reset();
+			}
+
+			void PrepareNextFrame()
+			{
+				prevKeyStates = keyStates;
+			}
+
+			bool IsKeyDown(uint16_t keyCode) const
+			{
+				size_t idx = static_cast<size_t>(keyCode);
+				return keyStates.test(idx) && !prevKeyStates.test(idx);
+			}
+
+			bool IsKeyHolding(uint16_t keyCode) const
+			{
+				size_t idx = static_cast<size_t>(keyCode);
+				return keyStates.test(idx) && prevKeyStates.test(idx);
+			}
+
+			bool IsKeyUp(uint16_t keyCode) const
+			{
+				size_t idx = static_cast<size_t>(keyCode);
+				return !keyStates.test(idx) && prevKeyStates.test(idx);
+			}
+
+		private:
+			std::bitset<256> keyStates;
+			std::bitset<256> prevKeyStates;
+		};
+	}
+}*/
 		};
 	}
 }
